6b47d0c8c0e9717221b7b542b21d0993198a35c9
Vasic2000 <Vasic2000@yandex.ru>
2019-04-23 14:31:13 +0300

    End Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Node> nod = new ArrayList<Node>();
+
+        Node[] nodes = {new Node(1,1,0),
+                new Node(2,2,0),
+                new Node(3,3,1),
+                new Node(4,4,3),
+                new Node(5,5,4),
+                new Node(6,6,0),
+                new Node(7,7,4),
+                new Node(8,8,4),
+        };
+
+        makeArray(nodes);
+    }
+
+    public static ArrayList<Node> makeArray(Node[] nodes){
+        ArrayList<Node> nod = new ArrayList<Node>();
+        for(int i = 0; i < nodes.length; i++)
+            if(nodes[i].parentId == 0)
+                nod.add(nodes[i]);
+            else
+                for(Node n : nodes)
+                    if(n.id == nodes[i].parentId)
+                        n.children.add(nodes[i]);
+        return nod;
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
+    int id;
+    int entutyId;
+    int parentId;
+    List<Node> children;
+
+    public Node(int id, int entutyId, int parentId) {
+        this.id = id;
+        this.entutyId = entutyId;
+        this.parentId = parentId;
+        this.children = new ArrayList<Node>();
+    }
 }
